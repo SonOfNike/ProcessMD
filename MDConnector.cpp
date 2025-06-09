dcd8e5343c9fffc991e3d54a7b81bc8fe5fced06
SonOfNike <chrisfreeman.trades@gmail.com>
2025-06-09 19:46:39 +0000

    add MDConnector

---
+++ MDConnector.cpp
@@ -0,0 +1,83 @@
+#include "MDConnector.h"
+
+void MDConnector::on_open(websocketpp::connection_hdl hdl, client* c) {
+    std::cout << "WebSocket connection opened!" << std::endl;
+    websocketpp::lib::error_code ec;
+    client::connection_ptr con = c->get_con_from_hdl(hdl, ec);
+ 
+    if (ec) {
+        std::cout << "Failed to get connection pointer: " << ec.message() << std::endl;
+        return;
+    }
+    auto auth_message_json = R"({"action": "auth", "key": "PKV7EJYKTZ61PJFW19I0", "secret": "OrN7TeuRsE0TtuyjywZoiPbq77V5SdFfUftcxaGM"})"_padded;
+    char *auth_message = auth_message_json.data();
+    c->send(con, auth_message, websocketpp::frame::opcode::text);
+
+    auto subscription_message_json = R"({"action":"subscribe","trades":["FAKEPACA"],"quotes":["FAKEPACA"]})"_padded;
+    char *subscription_message = subscription_message_json.data();
+    c->send(con, subscription_message, websocketpp::frame::opcode::text);
+}
+
+void MDConnector::on_message(websocketpp::connection_hdl, client::message_ptr msg) {
+    std::cout << "Received message: " << msg->get_payload() << std::endl;
+
+    simdjson::padded_string padded_json_string(msg->get_payload());
+
+    for(simdjson::dom::object obj : MDConnector::parser.parse(padded_json_string)){
+        bool is_MD = false;
+        for(const auto& key_value : obj) {
+            if(key_value.key == "T"){
+                std::string_view value = obj["T"].get_string();
+                if(value == "t"){
+                    is_MD = true;
+                    std::cout << "Received trade" << std::endl;
+                    continue;
+                }
+                else if(value == "q"){
+                    is_MD = true;
+                    std::cout << "Received quote" << std::endl;
+                    continue;
+                }
+                else{
+                    continue;
+                }
+            }
+        }
+    }
+}
+
+void MDConnector::on_fail(websocketpp::connection_hdl hdl) {
+    std::cout << "WebSocket connection failed!" << std::endl;
+}
+
+void MDConnector::on_close(websocketpp::connection_hdl hdl) {
+    std::cout << "WebSocket connection closed!" << std::endl;
+}
+
+void MDConnector::on_init() {
+    try {
+        c.set_access_channels(websocketpp::log::alevel::all);
+        c.clear_access_channels(websocketpp::log::alevel::frame_payload);
+        c.set_error_channels(websocketpp::log::elevel::all);
+        c.init_asio();
+      
+        c.set_message_handler(&on_message);
+        c.set_tls_init_handler(bind(&on_tls_init, hostname.c_str(), ::_1));
+       
+        c.set_open_handler(bind(&on_open, ::_1, &c));
+        c.set_fail_handler(bind(&on_fail, ::_1));
+        c.set_close_handler(bind(&on_close, ::_1));
+        c.set_error_channels(websocketpp::log::elevel::all);  // Enable detailed error logging
+        websocketpp::lib::error_code ec;
+        client::connection_ptr con = c.get_connection(uri, ec);
+        if (ec) {
+            std::cout << "Could not create connection because: " << ec.message() << std::endl;
+            return;
+        }
+        c.connect(con);
+       
+        c.run();
+    } catch (websocketpp::exception const & e) {
+        std::cout << "WebSocket Exception: " << e.what() << std::endl;
+    }
+}
---
+++ MDConnector.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <websocketpp/config/asio_client.hpp>
+#include <websocketpp/client.hpp>
+#include <iostream>
+#include "simdjson.h"
+#include <string>
+
+typedef websocketpp::client<websocketpp::config::asio_tls_client> client;
+typedef websocketpp::lib::shared_ptr<websocketpp::lib::asio::ssl::context> context_ptr;
+
+using websocketpp::lib::placeholders::_1;
+using websocketpp::lib::placeholders::_2;
+using websocketpp::lib::bind;
+
+class MDConnector {
+public:
+    void on_init();
+
+private:
+
+    static void on_open(websocketpp::connection_hdl hdl, client* c);
+    static void on_message(websocketpp::connection_hdl, client::message_ptr msg);
+    static void on_fail(websocketpp::connection_hdl hdl);
+    static void on_close(websocketpp::connection_hdl hdl);
+
+    client c;
+    std::string hostname = "stream.data.alpaca.markets/v2/test";
+    std::string uri = "wss://" + hostname;
+    static simdjson::dom::parser parser;
+
+    static context_ptr on_tls_init(const char * hostname, websocketpp::connection_hdl) {
+        context_ptr ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::sslv23);
+        try {
+            ctx->set_options(boost::asio::ssl::context::default_workarounds |
+                            boost::asio::ssl::context::no_sslv2 |
+                            boost::asio::ssl::context::no_sslv3 |
+                            boost::asio::ssl::context::single_dh_use);
+        } catch (std::exception& e) {
+            std::cout << "TLS Initialization Error: " << e.what() << std::endl;
+        }
+
+        return ctx;
+    }
+};
